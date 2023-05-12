3ff1fe12fc502a8adf1dc9759200e67583efa22e
Kgatliso Matema <kgatliso481@live.com>
2023-05-12 19:02:13 +0200

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter a string to see if it's a Palindrome: ");
+
+string name = Console.ReadLine();
+
+char[] nameArray = name.ToCharArray();
+
+Array.Reverse(nameArray);
+
+string reverse = new string(nameArray);
+
+if (name.Equals(reverse, StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine($"{name} is Palindrome");
+}
+else
+{
+    Console.WriteLine($"{name} is not Palindrome");
+}
+Console.ReadKey();
